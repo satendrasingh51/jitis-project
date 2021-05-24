52a64a0b1d2bcef91d66af904c313437c91168cf
satendra singh <mr.satendrasingh51@gmail.com>
2021-05-24 19:05:00 +0530

    kjdflk

--- routes/jitsi.js
+++ routes/jitsi.js
@@ -2,6 +2,7 @@ const express=require('express');
 const auth=require('../middleware/auth');
 const router=express();
 const users=require('../models/users');
+const crypto=require('crypto')
 
 
 /**
@@ -9,10 +10,17 @@ const users=require('../models/users');
  * @description Create room in jitsi
  * @access Private
  */
-router.get('/create/room', auth, async (req, res) => {
+router.get('/create/room', async (req, res) => {
     try {
-        const user=await users.findById(req.user.id).select('-password');
-        res.json({room: `https://jitsi.meest4bharat.net/${user.id}`})
+        crypto.randomBytes(32, (error, buffer) => {
+            if (error) {
+                console.log(error.msg)
+                res.json({msg: 'Server Error'})
+            }
+            const token=buffer.toString("hex")
+            res.json({room: `https://jitsi.meest4bharat.net/${token}`})
+        });
+        // const user=await users.findById(req.user.id).select('-password');
     } catch (error) {
         if (error) {
             return res.json({msg: [{msg: error.message}]})
