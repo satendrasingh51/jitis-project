c480d26d29298a1c07bbc44457905148c7f98d2c
satendra singh <mr.satendrasingh51@gmail.com>
2021-05-24 18:56:18 +0530

    kljflsdk

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', async (req, res) => {
+	res.send('Welcome To jitis App')
+})
+
 app.use('/api', usersRouter);
 app.use('/api', loginRouter);
 app.use('/api', jitsiRouter);
